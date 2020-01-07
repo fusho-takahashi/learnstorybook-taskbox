c7f1b389b4dd7015f039f63d032b6530ea556bc6
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-07 19:23:17 +0900

    task component の実装

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "task-item",
+  templateUrl: "./task.component.html",
+  styleUrls: ["./task.component.scss"]
+})
+export class TaskComponent implements OnInit {
+  constructor() {}
+
+  title: string;
+  @Input() task: any;
+  @Output() pinTask: EventEmitter<any> = new EventEmitter<void>();
+  @Output() archiveTask: EventEmitter<any> = new EventEmitter<void>();
+
+  ngOnInit() {}
+}
